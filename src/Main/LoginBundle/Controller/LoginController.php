8368aac5baff34fa0622d03691f62048ac84c3b9
Admin <Admin@Luis>
2014-12-29 15:27:27 -0600

    Login

--- src/Main/LoginBundle/Controller/LoginController.php
+++ src/Main/LoginBundle/Controller/LoginController.php
@@ -9,6 +9,7 @@ class LoginController extends Controller
     public function indexAction()
     {
         //return $this->render('MainLoginBundle:Default:index.html.twig', array('name' => $name));
+        
         return $this->render('MainLoginBundle:Login:index.html.twig', array('name' => 'demo'));
     }
 }
